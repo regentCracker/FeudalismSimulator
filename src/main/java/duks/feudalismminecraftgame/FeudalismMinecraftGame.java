54f2b3f6253a6208ef91f49fd75980aaefb540ab
regentCracker <105052870+regentCracker@users.noreply.github.com>
2023-04-09 17:00:41 +0300

    added //test comment

---
+++ src/main/java/duks/feudalismminecraftgame/FeudalismMinecraftGame.java
@@ -0,0 +1,16 @@
+package duks.feudalismminecraftgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FeudalismMinecraftGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //test
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
