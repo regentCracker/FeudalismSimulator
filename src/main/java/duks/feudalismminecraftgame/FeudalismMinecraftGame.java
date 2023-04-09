dcc19b73e4883d50e0943c92f668c4b80b373a3e
regentCracker <orenkosch@gmail.com>
2023-04-09 18:45:29 +0300

    added //2 comment

--- src/main/java/duks/feudalismminecraftgame/FeudalismMinecraftGame.java
+++ src/main/java/duks/feudalismminecraftgame/FeudalismMinecraftGame.java
@@ -1,17 +1,41 @@
 package duks.feudalismminecraftgame;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class FeudalismMinecraftGame extends JavaPlugin {
+public final class FeudalismMinecraftGame extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        //test
-        //2
+        System.out.println("ok");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
+
+    }
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e){
+        Block b = e.getBlock();
+        Player p = e.getPlayer();
+        Location l = b.getLocation();
+        double x = l.getX();
+        double y = l.getY();
+        double z = l.getZ();
+        getServer().broadcastMessage(b+"");
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        System.out.println("some1 has joined");
+        e.setJoinMessage(ChatColor.YELLOW+"Ilay 3 joined the game");
 
     }
 }
---
+++ src/main/java/duks/feudalismminecraftgame/Wallet.java
@@ -0,0 +1,4 @@
+package duks.feudalismminecraftgame;
+
+public class Wallet {
+}
