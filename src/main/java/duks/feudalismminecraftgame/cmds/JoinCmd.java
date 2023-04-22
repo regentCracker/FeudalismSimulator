97444238fe7c9be2493465d56fcddc5863d03399
regentCracker <orenkosch@gmail.com>
2023-04-22 20:25:27 +0300

    added the joincmd template

--- src/main/java/duks/feudalismminecraftgame/cmds/JoinCmd.java
+++ src/main/java/duks/feudalismminecraftgame/cmds/JoinCmd.java
@@ -1,21 +1,19 @@
-package duks.feudalismminecraftgame;
+package duks.feudalismminecraftgame.cmds;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GeneralCommand implements CommandExecutor{
+public class JoinCmd implements CommandExecutor {
     JavaPlugin plugin;
-    public GeneralCommand(JavaPlugin plugin){
+    public JoinCmd(JavaPlugin plugin){
         this.plugin=plugin;
     }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 
-        plugin.getServer().broadcastMessage("npc moment");
         if(sender instanceof Player){
             System.out.println("aaa");
             plugin.getServer().broadcastMessage("npc moment");
